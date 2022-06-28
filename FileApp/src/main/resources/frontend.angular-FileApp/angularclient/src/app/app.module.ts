2ac4709cda7f4459916021389285383e50d1c4e2
FilipLiljebris <66790921+Liljebris@users.noreply.github.com>
2022-06-28 15:19:18 +0200

    Basic UI
    Added a basic UI with 2 buttons and 2 text field, one for posting text into file and another to receive it.

--- FileApp/src/main/resources/frontend.angular-FileApp/angularclient/src/app/app.module.ts
+++ FileApp/src/main/resources/frontend.angular-FileApp/angularclient/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialExampleModule } from '../material.module';
+import { TextFieldAutosizeTextareaExample } from './text-field-autosize-textarea-example';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextFieldAutosizeTextareaExample
+
+
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    TextFieldAutosizeTextareaExample,
+]
+
 })
 export class AppModule { }
